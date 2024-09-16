e9d23ee8ab3c2508c57d3af55bc9b763584bd198
Simpronic <marcdifiandra98@gmail.com>
2024-09-16 14:03:51 +0200

    Update models_simulation_ss.py

--- models_simulation_ss.py
+++ models_simulation_ss.py
@@ -10,6 +10,7 @@ from DRAMA_ScaleSim.scale import scale
 
 # SS1 -> ScaleSim 1 (+drama)
 # SS2 -> Scalesim 2
+
 def getRunName(cfg_file_path):
     config = cp.ConfigParser()
     config.read(cfg_file_path)
@@ -27,7 +28,7 @@ def getMaxAndAvgFileWeight_SS1(folder,exlusion_list):
          n_files += 1
          if size > max:
             max = size
- return sum/n_files,max,(n_files-5)/6
+ return sum,n_files,sum/n_files,max,(n_files-5)/6
 
 def getMaxAndAvgFileWeight_SS2(folder):
    layers_f= [nome for nome in os.listdir(folder) if os.path.isdir(os.path.join(folder,nome))]
@@ -44,7 +45,7 @@ def getMaxAndAvgFileWeight_SS2(folder):
        if max_f_w > max_find:
           max_find = max_f_w
    print("Il numero totale di file generati sono: "+str(total_n_file))
-   return totalSum/total_n_file,max_find,total_n_file/6
+   return totalSum,n_files,totalSum/total_n_file,max_find,total_n_file/6
 
 def deleteAllCSV(folder,exlusion_list):
    files = os.listdir(folder)
@@ -74,15 +75,17 @@ def startSimulation_SS2(cfg_path,topologyFolder,result_file):
        s = scalesim(save_disk_space=False, verbose=True, config=cfg_path,topology=topologyFolder+'\\'+files[0])
        top_module_name = "test_run"+str(i)
        topologyFileRun = files[i] 
-       data_array.append(simulation_topologyDta(name = files[i].split('.')[0],n_layer= 0,execution_time=0,avg_file_weight=0,max_file_weight=0))
+       data_array.append(simulation_topologyDta(name = files[i].split('.')[0],n_layer= 0,execution_time=0,avg_file_weight=0,max_file_weight=0,weight=0,n_files=0))
        s.set_params(config_filename=cfg_path,topology_filename=topologyFolder+'\\'+topologyFileRun)
        start_time = time.time()
        s.run_scale(top_path=top_module_name)
        end_time = time.time()
-       avg_f_w,max_f_w,n_layers = getMaxAndAvgFileWeight_SS2(top_module_name+'\\'+getRunName(cfg_path))
+       weight,number_of_files,avg_f_w,max_f_w,n_layers = getMaxAndAvgFileWeight_SS2(top_module_name+'\\'+getRunName(cfg_path))
        data_array[i].setAvgFweight(avg_f_w)
        data_array[i].setMaxFweight(max_f_w)
        data_array[i].setLayerNumbers(n_layers)
+       data_array[i].setWeight(weight)
+       data_array[i].setNumberOfFile(number_of_files)
        if os.path.exists(top_module_name): 
         shutil.rmtree(top_module_name)
        data_array[i].setExecTime(end_time-start_time)
@@ -102,15 +105,17 @@ def startSimulation_SS1(cfg_path,topologyFolder,result_file):
        s = scale(save = False, sweep = False)
        topologyFileRun = files[i] 
        s.setFlagsByFunc(cfg_path,topologyFolder+'\\'+topologyFileRun)
-       data_array.append(simulation_topologyDta(name = files[i].split('.')[0],n_layer= 0,execution_time=0,avg_file_weight=0,max_file_weight=0))
+       data_array.append(simulation_topologyDta(name = files[i].split('.')[0],n_layer= 0,execution_time=0,avg_file_weight=0,max_file_weight=0,weight=0,n_files=0))
        start_time = time.time()
        s.run_scaleForMisuration()
        end_time = time.time()
        data_array[i].setExecTime(end_time-start_time)
-       avg_f_w,max_f_w,n_layers =  getMaxAndAvgFileWeight_SS1("Models",files)
+       weight,number_of_files,avg_f_w,max_f_w,n_layers =  getMaxAndAvgFileWeight_SS1("Models",files)
        data_array[i].setAvgFweight(avg_f_w)
        data_array[i].setMaxFweight(max_f_w)
        data_array[i].setLayerNumbers(n_layers)
+       data_array[i].setWeight(weight)
+       data_array[i].setNumberOfFile(number_of_files)
        deleteAllCSV("Models",files)
        del s
     writeDataOnCSV(data_array,result_file+"_SS1.csv")
